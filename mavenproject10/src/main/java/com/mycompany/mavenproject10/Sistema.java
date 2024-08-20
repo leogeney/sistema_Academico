3571f6358fd0b44a040a0f5a523cf38ce0f42e20
leogeney <ljgeneyl@ufpso.edu.co>
2024-08-19 20:57:06 -0500

    arreglo

---
+++ mavenproject10/src/main/java/com/mycompany/mavenproject10/Sistema.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject10;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Sistema {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        int array[] = new int[5];
+        
+        for (int i = 0; i < 5; i++) {
+            array[i] = entrada.nextInt();
+            
+            
+            
+        }
+    }
+}
